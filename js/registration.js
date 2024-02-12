384373e7af0abe7da13d213a2e08925772b2c74b
KanVull <minirorus2@gmail.com>
2024-02-12 15:14:40 +0400

    Add password jQuery plug-in

--- js/registration.js
+++ js/registration.js
@@ -24,4 +24,11 @@ $(function() {
     $(document).on('change', '#checkTermsAgree', function() {
         checkButtonState();
     });
+    $('.pr-password').passwordRequirements({
+        numCharacters: 8,
+        useLowercase: true,
+        useUppercase: true,
+        useNumbers: true,
+        useSpecial: true,
+    });
 });
